713261252134f72684b5f8e43ba6bd2f21270515
SaranshOp <kathalsaransh@gmail.com>
2025-02-22 18:25:01 +0530

    updated users dashboard

--- nextjs-deployed/src/lib/types.ts
+++ nextjs-deployed/src/lib/types.ts
@@ -1,54 +1,54 @@
 export enum UserRole {
-    ADMIN = "ADMIN",
-    OPERATOR = "OPERATOR",
-    USER = "USER"
-  }
-  
-  export enum VehicleType {
-    BIKE = 1,
-    CAR = 2,
-    TRUCK = 3
-  }
-  
-  export enum PaymentStatus {
-    PENDING = "PENDING",
-    COMPLETED = "COMPLETED"
-  }
-  
-  export interface User {
-    username: string;
-    role: UserRole;
-  }
-  
-  export interface ParkingActivity {
-    registration: string;
-    entry_time: Date;
-    exit_time?: Date;
-    amount_paid: number;
-  }
-  
-  export interface Vehicle {
-    reg_num: string;
-    vehicle_type: VehicleType;
-    entry_time: Date;
-    exit_time?: Date;
-    payment_status: PaymentStatus;
-    assigned_spots: [number, number][];
-  }
-  
-  export interface ParkingSpot {
-    floor_id: number;
-    spot_id: number;
-    is_occupied: boolean;
-    vehicle?: Vehicle;
-    vehicle_type?: VehicleType;
-  }
+  ADMIN = "ADMIN",
+  OPERATOR = "OPERATOR",
+  USER = "USER",
+}
 
-  export function getSpotsNeeded(type: VehicleType): number {
-    return type <= VehicleType.CAR && type >= VehicleType.BIKE ? 1 : 2;
-  }
-  
-  export interface VehicleInfo {
+export enum VehicleType {
+  BIKE = 1,
+  CAR = 2,
+  TRUCK = 3,
+}
+
+export function getSpotsNeeded(type: VehicleType): number {
+  return type <= VehicleType.CAR && type >= VehicleType.BIKE ? 1 : 2;
+}
+
+export enum PaymentStatus {
+  PENDING = "PENDING",
+  COMPLETED = "COMPLETED",
+}
+
+export interface User {
+  username: string;
+  role: UserRole;
+}
+
+export interface ParkingActivity {
+  registration: string;
+  entry_time: Date;
+  exit_time?: Date;
+  amount_paid: number;
+}
+
+export interface Vehicle {
+  reg_num: string;
+  vehicle_type: VehicleType;
+  entry_time: Date;
+  exit_time?: Date;
+  payment_status: PaymentStatus;
+  assigned_spots: [number, number][];
+}
+
+export interface ParkingSpot {
+  floor_id: number;
+  spot_id: number;
+  is_occupied: boolean;
+  vehicle?: Vehicle;
+  vehicle_type?: VehicleType;
+}
+
+export interface VehicleInfo {
   registration: string;
   type: string;
   entry_time: Date;
